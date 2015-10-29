b6162bc350b45b4fa3b14d0afa987e6526faf279
AlexLYZ <347851258@qq.com>
2015-10-29 23:17:55 +0800

    spider login
    这里知乎已经改为验证码登录,还需改进才能使用python爬虫登录知乎

--- buildopener.py
+++ buildopener.py
@@ -1,5 +1,21 @@
 import urllib.request
 import http.cookiejar
+import gzip
+import re
+
+def getXSRF(data):
+	xsrf = re.compile('name="_xsrf" value="(.*?)"',flags = 0)
+	strlist = xsrf.findall(data)
+	return strlist[0]
+
+def ungzip(data):
+    try:        # 尝试解压
+        print('正在解压.....')
+        data = gzip.decompress(data)
+        print('解压完毕!')
+    except:
+        print('未经压缩, 无需解压')
+    return data
 
 #用报头创建opener
 def makeMyopener(head = {
@@ -25,7 +41,27 @@ def saveFile(data):
 
 oper = makeMyopener()
 #用opener对象的open函数打开URL
-response = oper.open('http://www.baidu.com/',timeout = 1000)
+response = oper.open('http://www.zhihu.com/',timeout = 1000)
 data = response.read()
-saveFile(data)
-print(data)
+ungzipeddata = ungzip(data)
+xsrf = getXSRF(data.decode('utf-8'))
+
+url += 'login'
+id = '******.com'
+password = '********'
+postDict = {
+	'_xsrf':xsrf,
+	'email':id,
+	'password':password,
+	'rememberme':'y'
+}
+postData = urllib.parse.urlencode(postDict).encode()
+opurl = oper.open(url,postData)
+data = opurl.read()
+data = ungzip(data)
+
+print(data.decode())
+#print(ungzipeddata)
+print(xsrf)
+#saveFile(data)
+#print(data)
