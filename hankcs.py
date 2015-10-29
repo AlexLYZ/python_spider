d398acd25eb6d679a6e0cb0fcc701319229e883d
AlexLYZ <347851258@qq.com>
2015-10-29 21:27:37 +0800

    autoskip
    sometimes the url is not reachable , so we skip to a available url

--- hankcs.py
+++ hankcs.py
@@ -15,7 +15,12 @@ while queue:
 	visited |={url}
 	print('已经抓取：'+str(cnt)+'正在抓取<---'+url)
 	cnt+=1
-	response = urllib.request.urlopen(url)
+	try:
+		#urllib.request.urlopen(url, data=None, [timeout, ]*, cafile=None, capath=None, cadefault=False, context=None)
+		#The optional timeout parameter specifies a timeout in seconds for blocking operations like the connection attempt (if not specified, the global default timeout setting will be used). 
+		response = urllib.request.urlopen(url,timeout = 2)
+	except:
+		continue
 	if 'html' not in response.getheader('Content-Type'):
 		continue
 	try:
--- spider.py
+++ spider.py
@@ -1,7 +1,16 @@
 import urllib.request
+import re
 #返回http.client.HTTPResponse对象
 #urllib.request.urlopen()抓取指定url的页面
-response = urllib.request.urlopen("http://www.baidu.com/")
+response = urllib.request.urlopen(" http://baidu.com")
 #response = urllib.urlopen("http://www.baidu.com/")
-html = response.read()
+#data = response.getheader('http-equiv')
+html = response.read().decode('UTF-8')
+pattern = re.compile('meta http-equiv="refresh" content="0;url=(.+?)"')
+for x in pattern.findall(html):
+	print(x)
+	response = urllib.request.urlopen(x)
+	data = response.read()
+	print(data)
 print (html)
+#print(data)
