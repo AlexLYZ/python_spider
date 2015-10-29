c5ac770afebef139a4ebae417d0df0b3f0e4945e
AlexLYZ <347851258@qq.com>
2015-10-29 22:18:35 +0800

    write HTML to file

--- buildopener.py
+++ buildopener.py
@@ -16,9 +16,16 @@ def makeMyopener(head = {
 		header.append(elem)
 	opener.addheaders = header
 	return opener
-	
+#写文件操作只包含三步，1：打开文件（指定打开方式）；2：数据写入文件	；3：关闭文件
+def saveFile(data):
+	save_path = 'D:\\temp.out'
+	f_obj = open(save_path,'wb')#w以ascii文件打开文件，wb用于打开二进制文件
+	f_obj.write(data)
+	f_obj.close()
+
 oper = makeMyopener()
 #用opener对象的open函数打开URL
 response = oper.open('http://www.baidu.com/',timeout = 1000)
 data = response.read()
+saveFile(data)
 print(data)
