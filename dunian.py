cd081fc8fc189da291ebf0fdd565a4f1257c78eb
AlexLYZ <347851258@qq.com>
2015-10-29 18:09:35 +0800

    comments

--- dunian.py
+++ dunian.py
@@ -3,6 +3,7 @@ import urllib.request
 
 data={}
 data['word']='最美 女星'
+#urllib.parse库对普通字符串转符合url的字符串
 url_values = urllib.parse.urlencode(data)
 url = "http://www.baidu.com/s?"
 full_url=url+url_values
--- spider.py
+++ spider.py
@@ -1,5 +1,6 @@
 import urllib.request
 #返回http.client.HTTPResponse对象
+#urllib.request.urlopen()抓取指定url的页面
 response = urllib.request.urlopen("http://www.baidu.com/")
 #response = urllib.urlopen("http://www.baidu.com/")
 html = response.read()
